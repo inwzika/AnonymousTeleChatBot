123b11e051a036e02a48e584fcec83f4af26ebe4
inwzika <nazaninnurollahii@gmail.com>
2023-08-04 11:41:23 +0430

    add logger to log messages in file

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -3,8 +3,11 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Bot extends TelegramLongPollingBot {
@@ -12,7 +15,7 @@ public class Bot extends TelegramLongPollingBot {
     private final HashMap<String, String> userIdMap = new HashMap<>(); //key = username , value= id
 
     private final HashMap<String, String> twoWayUsers = new HashMap<>();// key= id sender - value= Id receiver
-
+    private final Logger logger = new Logger();
 
     @Override
     public void onUpdateReceived(Update update) {
@@ -21,29 +24,23 @@ public class Bot extends TelegramLongPollingBot {
         String userId = user.getId().toString();
         if (msg.isCommand()) {
             if (msg.getText().equals("/start")) {
-                sendText(userId, "Welcome to my Bot");
+                ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+                ArrayList<KeyboardRow> rows = new ArrayList<>();
+                KeyboardRow keyboardRow = new KeyboardRow();
+                keyboardRow.add("Connect to user");
+                rows.add(keyboardRow);
+                replyKeyboardMarkup.setKeyboard(rows);
+                sendMessage(userId, "Welcome to my Bot");
                 if (user.getUserName() != null) {
                     userIdMap.put(user.getUserName(), userId);
                 }
             }
         } else {
-            char isValid = msg.getText().charAt(0);
-            if (isValid == '@') {
-                String userId2 = userIdMap.get(msg.getText().substring(1));
-                twoWayUsers.put(userId, userId2);
-                twoWayUsers.put(userId2, userId);
-            } else {
-                if (twoWayUsers.containsKey(userId)) {
-                    String userId2 = twoWayUsers.get(userId);
-                    sendText(userId2, msg.getText());
-                } else {
-                    sendText(userId, "Please enter a valid username starting with @");
-                }
-            }
+
+            sendRequest(msg);
         }
         logMessage(msg, true);
     }
-
 
     private void logMessage(Message message) {
         System.out.println("Name: " + message.getFrom().getFirstName() + " - Text: " + message.getText());
@@ -51,12 +48,14 @@ public class Bot extends TelegramLongPollingBot {
 
     private void logMessage(Message message, boolean printId) {
         if (printId) {
-            System.out.println("Id: " + message.getFrom().getId() + " - Name: " + message.getFrom().getFirstName() + " - Text: " + message.getText());
+            String s = "Id: " + message.getFrom().getId() + " - Name: " + message.getFrom().getFirstName() + " - Text: " + message.getText();
+            System.out.println(s);
+            logger.log(s);
         } else
             logMessage(message);
     }
 
-    public void sendText(String who, String what) {
+    public void sendMessage(String who, String what) {
 //        SendMessage sm = new SendMessage(who.toString(),what);
         SendMessage sm = SendMessage.builder()
                 .chatId(who) //Who are we sending a message to
@@ -77,4 +76,27 @@ public class Bot extends TelegramLongPollingBot {
     public String getBotToken() {
         return Main.token;
     }
+
+    private void sendRequest(Message msg){
+        String userId = msg.getFrom().getId().toString();
+        char isValid = msg.getText().charAt(0);
+        if (isValid == '@') {
+            connectToUser(msg, userId);
+        } else {
+            if (twoWayUsers.containsKey(userId)) {
+                String userId2 = twoWayUsers.get(userId);
+                sendMessage(userId2, msg.getText());
+            } else {
+                sendMessage(userId, "Please enter a valid username starting with @");
+            }
+        }
+    }
+
+    private void connectToUser(Message msg, String userId) {
+        String userId2 = userIdMap.get(msg.getText().substring(1));
+        twoWayUsers.put(userId, userId2);
+        twoWayUsers.put(userId2, userId);
+    }
+
+
 }
---
+++ src/main/java/Logger.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Formatter;
+
+public class Logger {
+    private String fileName;
+    private static final String DEFAULT_FILE_NAME = "log.txt";
+
+    public Logger(){
+        this.fileName = DEFAULT_FILE_NAME;
+    }
+
+    public Logger(String fileName){
+        this.fileName = fileName;
+    }
+
+    public void log(String message){
+        try {
+            FileWriter fileWriter = new FileWriter(fileName,true);
+            Formatter formatter = new Formatter(fileWriter);
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+            String time = dtf.format(LocalDateTime.now());
+            formatter.format("Time: "+ time+ " -  " +message + "\n");
+            formatter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
